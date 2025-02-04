a68a86354497d598a76c810e6a1f711470c07f10
Abubakar-1 <abubakaruabdulkadir@gmail.com>
2025-02-04 12:04:20 +0100

    soks

--- src/app/admin/donation-campaigns/page.tsx
+++ src/app/admin/donation-campaigns/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import { AdminProtection } from "@/components/AdminProtecton";
 import { getDonationCampaigns } from "./actions";
 import { DonationCampaignForm } from "./donation-campaigns-form";
 import { DonationCampaignList } from "./donation-campaigns-list";
 
 export default async function DonationCampaignsPage() {
-  const initialCampaigns = await getDonationCampaigns();
+  const getCampaigns = async () => {
+    return await getDonationCampaigns();
+  };
+
+  const initialCampaigns = await getCampaigns();
+
   return (
     <AdminProtection>
       <div className="space-y-6">
