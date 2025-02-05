24c04fd44d0b735e8e7a19ee0945202ffc48b34f
Abubakar-1 <abubakaruabdulkadir@gmail.com>
2025-02-05 12:49:21 +0100

    kk

--- src/components/PaystackButton.tsx
+++ src/components/PaystackButton.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState } from "react";
-// import { usePaystackPayment } from "react-paystack";
+import { usePaystackPayment } from "react-paystack";
 import { Button } from "@/components/ui/button";
 
 interface PaystackButtonProps {
@@ -15,9 +15,9 @@ interface PaystackButtonProps {
 export function PaystackButton({
   amount,
   email,
-}: // onSuccess,
-// onClose,
-PaystackButtonProps) {
+  onSuccess,
+  onClose,
+}: PaystackButtonProps) {
   const [isLoading, setIsLoading] = useState(false);
 
   const config: any = {
@@ -26,27 +26,26 @@ PaystackButtonProps) {
     amount: amount * 100, // Paystack expects amount in kobo
     publicKey: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY!,
   };
-  console.log(config);
 
-  // const initializePayment = usePaystackPayment(config);
+  const initializePayment = usePaystackPayment(config);
 
   const handlePayment = () => {
     try {
       setIsLoading(true); // Start the loading state
 
-      // initializePayment({
-      //   onSuccess: (response: any) => {
-      //     // Payment successful
-      //     setIsLoading(false);
-      //     console.log(response);
-      //     onSuccess(response.reference);
-      //   },
-      //   onClose: () => {
-      //     // Payment modal closed
-      //     setIsLoading(false);
-      //     onClose();
-      //   },
-      // });
+      initializePayment({
+        onSuccess: (response: any) => {
+          // Payment successful
+          setIsLoading(false);
+          console.log(response);
+          onSuccess(response.reference);
+        },
+        onClose: () => {
+          // Payment modal closed
+          setIsLoading(false);
+          onClose();
+        },
+      });
     } catch (error) {
       console.error("Error during payment initialization:", error);
       setIsLoading(false); // Ensure loading state is reset in case of error
